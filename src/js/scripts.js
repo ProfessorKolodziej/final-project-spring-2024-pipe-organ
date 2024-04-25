19fa19d7bb1c202c84d63d1eace4d070c79eb69a
LydiaSolodiuk <157635059+LydiaSolodiuk@users.noreply.github.com>
2024-04-24 20:27:01 -0400

    4/24 Submission

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,45 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+
+//Visual Portion Learn
+const topSection = document.querySelector('#top');
+const middleSection = document.querySelector('#middle');
+const bottomSection = document.querySelector('#bottom');
+
+function showInformation() {
+	const shownItems = document.querySelectorAll('.show');
+}
+for (item of shownItems) {
+	item.classList.remove('show');
+}
+
+topSection.addEventListener("onmouseover", showInformation);
+middleSection.addEventListener("onmouseover", showInformation);
+bottomSection.addEventListener("onmouseover", showInformation);
+
+//Audio portion Listen
+function playAudio() {
+	let audio = document.getElementById("Principal-16");
+	playAudio();
+}
+
+function playAudio() {
+	let audio = document.getElementById("Vox");
+	playAudio();
+}
+
+function playAudio() {
+	let audio = document.getElementById("Principal-8");
+	playAudio();
+}
+
+function playAudio() {
+	let audio = document.getElementById("Nazard");
+	playAudio();
+}
+
+function playAudio() {
+	let audio = document.getElementById("Trumpet");
+	playAudio();
+}
