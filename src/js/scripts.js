c995be160ef358edaa0f2fde4dacacf530c2fcb8
LydiaSolodiuk <157635059+LydiaSolodiuk@users.noreply.github.com>
2024-04-30 22:32:49 -0400

    Gradescope submiddion

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -9,15 +9,71 @@
 
 // Listen
 
-let button = document.getElementsById("P16");
-button.addEventListener("click", myFunction()); {
-	const audio = document("P16", onclick);
-	console.log("myFunction")
+//let button = document.getElementsById("P16");
+//button.addEventListener("click", myFunction()); {
+//const audio = document("P16", onclick);
+//console.log("myFunction")
+//}
 
+//let document = document.getElementsByClassName("VC");
+//button.addEventListener("click", myFunction()); {
+//const audio = document("VC", onclick);
+//console.log("VC")
+//}
+
+//let audio = new Audio('Audio/Ped_OpenDiap16_arp.mp3');
+//audio.load();
+//audio.play();
+
+//let play = function () { document.getElementsByClassName("P16").play() }
+
+//const myP16 = document.getElementById("P16");
+//const firstButton = document.getElementById("Pedal16");
+
+//{
+//firstButton.addEventListener("click", function ());
+//}
+
+//function myP16(onclick)
+//audio.play();
+
+
+//const audio = new
+//Audio("Audio/Ped_OpenDiap16_arp.mp3");
+//const buttons = document.querySelectorAll("button");
+
+//buttons.forEach("P16" => {
+//button.addEventListener("click", () => {
+//audio.play();
+//});
+//});
+
+//const audio = newAudio("Audio/Rec_Celeste_arp.mp3")
+//buttons.forEach(button, VC{
+//button.addEventListener("click", ())
+//audio.play();
+//}
+
+//function play() {
+	//const audio = document.getElementById("P16");
+	//audio.play();
 }
 
-let document = document.getElementsByClassName("VC");
-button.addEventListener("click", myFunction()); {
-	const audio = document("VC", onclick);
-	console.log("VC")
-}
+//function play() {
+//const audio = document.getElementById("VC");
+//audio2.play();
+
+//}
+
+const playSound = (Audio / Ped_OpenDiap16_arp.mp3) => {
+	const audio = new Audio();
+	audio.src = Audio / Ped_OpenDiap16_arp.mp3;
+	audio.play();
+};
+
+document.querySelectorAll("input").forEach((Input))=>
+button.addEventListener("click", () => {
+	playSound(Input);
+});
+
+
